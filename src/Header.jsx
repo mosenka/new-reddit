81bcf87c55964a70f1428ccaefa00493bfb489e4
Алина Четкарёва <mosenka91@mail.ru>
2022-03-25 11:13:24 +0400

    init commit

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styles from './header.less';
+
+export function Header(){
+	return (
+		<header className={styles.example}>
+			<h1>Hello React</h1>
+		</header>
+	);
+}
